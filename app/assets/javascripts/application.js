52a1ff88ca801f1cf195599f7dc8e4d95c31a2fb
lilu <lilu.life@gmail.com>
2012-12-12 21:43:08 +0800

    reset frondend related gems, modify base assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,20 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
-//= require bootstrap
+//= require bootstrap-all
 //= require_tree .
-
-$.datepicker.setDefaults({
-  dateFormat: 'yy-mm-dd',
-  dayNamesMin: [ '日', '一', '二', '三', '四', '五', '六' ],
-  monthNames: [ '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' ],
-  yearSuffix: "年",
-  changeYear: true,
-  showMonthAfterYear: true
-})
-
-$(function(){
-  $(".datepicker").datepicker();
-});
-
